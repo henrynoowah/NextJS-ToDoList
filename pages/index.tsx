71c60861f7bd6a82eaa947128f92b938b870bca4
Henry <henrynoowah@gmail.com>
2022-01-21 01:10:17 +0900

    [FIRST] first commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  font-style: italic;
+`;
+
+const index = () => {
+  return <Container>hello Typescript</Container>;
+};
+
+export default index;
