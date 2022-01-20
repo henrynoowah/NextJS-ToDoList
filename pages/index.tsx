562edf883ea506257c2be72c09591ca8a7c96e80
Henry <henrynoowah@gmail.com>
2022-01-21 01:38:47 +0900

    [SETTING] Add styled-component global font

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,15 @@ import styled from "styled-components";
 
 const Container = styled.div`
   font-style: italic;
+  padding: 20px;
 `;
 
 const index = () => {
-  return <Container>hello Typescript</Container>;
+  return (
+    <Container>
+      <h1>hello Typescript</h1>
+    </Container>
+  );
 };
 
 export default index;
--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -1,9 +1,18 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, css } from "styled-components";
+import reset from "styled-reset";
+
+const globalStyle = css`
+  ${reset}
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    font-family: Noto Sans, Noto Sans KR;
+  }
+`;
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-  }
-`
+  ${globalStyle}
+`;
 
 export default GlobalStyle;
