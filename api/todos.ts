074ef5049b04432724127f53ff6cb68c37ed5a76
Henry <henrynoowah@gmail.com>
2022-01-22 15:59:22 +0900

    [HOTFIX] globalStyle font using css import

---
+++ api/todos.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    return res.send("Hello Next!")
+  }
+
+  res.statusCode == 405;
+  console.log(res.statusCode);
+  return res.end();
+}
--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -2,13 +2,14 @@ import { createGlobalStyle, css } from "styled-components";
 import reset from "styled-reset";
 
 const globalStyle = css`
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300&display=swap');
   ${reset};
 
   * {
     box-sizing: border-box;
   }
   body {
-    font-family: Noto Sans, Noto Sans KR;
+    font-family: 'Noto Sans KR', sans-serif;
   }
 `;
 
